4562405a5c3839bce70f5b32bacb3d2731fddc6b
GOPAL THAKUR <gopalk557@gmail.com>
2022-01-23 20:49:53 +0530

    repeated twice like 11 22 33 99

---
+++ Practice7-5.sh
@@ -0,0 +1,14 @@
+#! /bin/bash 
+declare -a reptnum
+c=0
+for i in {1..100}
+
+do
+   if [ $((i%10)) -eq $((i/10)) ]
+   then
+    reptnum[(( c++ ))]=$i
+   #echo "$i"
+   fi
+done
+
+echo ${reptnum[*]}
